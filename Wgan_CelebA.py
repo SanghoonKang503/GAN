ea7b90bf042b3b85a545ddac0949e249570b2858
Albert <albert.no0@gmail.com>
2019-10-16 17:10:53 +0900

    ADD: add gan specific python file

--- Wgan_CelebA.py
+++ Wgan_CelebA.py
@@ -10,74 +10,7 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 
-
-# G(z)
-# Generate fake image
-class generator(nn.Module):
-    # initializers
-    def __init__(self):
-        super(generator, self).__init__()
-        self.deconv1 = nn.ConvTranspose2d(100, 1024, 4, 1, 0)
-        self.deconv1_bn = nn.BatchNorm2d(1024)
-        self.deconv2 = nn.ConvTranspose2d(1024, 512, 4, 2, 1)
-        self.deconv2_bn = nn.BatchNorm2d(512)
-        self.deconv3 = nn.ConvTranspose2d(512, 256, 4, 2, 1)
-        self.deconv3_bn = nn.BatchNorm2d(256)
-        self.deconv4 = nn.ConvTranspose2d(256, 128, 4, 2, 1)
-        self.deconv4_bn = nn.BatchNorm2d(128)
-        self.deconv5 = nn.ConvTranspose2d(128, 3, 4, 2, 1)
-
-    # weight_init
-    def weight_init(self, mean, std):
-        for m in self._modules:
-            normal_init(self._modules[m], mean, std)
-
-    # forward method
-    def forward(self, input):
-        x = F.relu(self.deconv1_bn(self.deconv1(input)))
-        x = F.relu(self.deconv2_bn(self.deconv2(x)))
-        x = F.relu(self.deconv3_bn(self.deconv3(x)))
-        x = F.relu(self.deconv4_bn(self.deconv4(x)))
-        x = torch.tanh(self.deconv5(x))
-
-        return x
-
-
-class discriminator(nn.Module):
-    # initializers
-    def __init__(self):
-        super(discriminator, self).__init__()
-        self.conv1 = nn.Conv2d(3, 128, 4, 2, 1)
-        self.conv1_bn = nn.BatchNorm2d(128)
-        self.conv2 = nn.Conv2d(128, 256, 4, 2, 1)
-        self.conv2_bn = nn.BatchNorm2d(256)
-        self.conv3 = nn.Conv2d(256, 512, 4, 2, 1)
-        self.conv3_bn = nn.BatchNorm2d(512)
-        self.conv4 = nn.Conv2d(512, 1024, 4, 2, 1)
-        self.conv4_bn = nn.BatchNorm2d(1024)
-        self.conv5 = nn.Conv2d(1024, 1, 4, 1, 0)
-
-
-    # weight_init
-    def weight_init(self, mean, std):
-        for m in self._modules:
-            normal_init(self._modules[m], mean, std)
-
-    # forward method
-    def forward(self, input):
-        x = F.leaky_relu(self.conv1_bn(self.conv1(input)), 0.2)
-        x = F.leaky_relu(self.conv2_bn(self.conv2(x)), 0.2)
-        x = F.leaky_relu(self.conv3_bn(self.conv3(x)), 0.2)
-        x = F.leaky_relu(self.conv4_bn(self.conv4(x)), 0.2)
-        x = self.conv5(x)
-
-        return x
-
-# Initailize weight of CNN
-def normal_init(m, mean, std):
-    if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
-        m.weight.data.normal_(mean, std)
-        m.bias.data.zero_()
+from sang_gan import *
 
 
 fixed_z_ = torch.randn((5 * 5, 100)).view(-1, 100, 1, 1)  # fixed noise
