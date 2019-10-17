c9c5513f5f87a39b8f4814ffc7ce0b4117bb9448
SanghoonKang503 <puppy503@naver.com>
2019-10-17 16:03:42 +0900

    ADD: import argparse ADD: add parser arguments

--- sang_main.py
+++ sang_main.py
@@ -6,7 +6,8 @@ import imageio
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
+import torch.optim as optim\
+import argparse
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 
@@ -16,11 +17,18 @@ from sang_utils import *
 
 
 # training parameters
-lr = 0.00005
-train_epoch = 200
-img_size = 64
+parser=argparse.ArgumentParser()
+parser.add_argument("--n_epochs", type=int, default=100, help= "Running Iterations")
+parser.add_argument("--batch_size", type=int, default=64, help= "Size of the Batches")
+parser.add_argument("--lr", type=int, default=0.0001, help="Adam Learning rate")
+parser.add_argument("--b1", type=int, default=0.5, help="Momentum of Adam beta1")
+parser.add_argument("--b2", type=int, default=0.999, help="Momentum of Adam beta2")
+parser.add_argument("--img_size", type=int, default=64, help="Size of input Image")
+parser.add_argument("--n_critic", type=int, default=5, help="Number of training step of Discriminator")
+parser.add_argument("--lambda", type=int, default=10, help="Lambda of Gradient Descent")
 
-batch_sizes = 64
+opt= parser.parse_args()
+
 data_dir = 'resized_celebA'  # this path depends on your computer
 
 # put image data into data_loader
