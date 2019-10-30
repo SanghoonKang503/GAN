fe5b4603f48b3cef0ca10ef8c1ccb29f20607ddc
SanghoonKang503 <puppy503@naver.com>
2019-10-30 17:56:15 +0900

    ADD : add print options

--- sang_main.py
+++ sang_main.py
@@ -11,8 +11,9 @@ if __name__ == "__main__":
     parser.add_argument("--img_size", type=int, default=64, help="Size of input Image")
     parser.add_argument("--n_critic", type=int, default=5, help="Number of training step of Discriminator")
     parser.add_argument("--lambda_gp", type=int, default=10, help="Lambda for gradient penalty")
-    parser.add_argument("--dataset", default='celebA', help='cifar10, celebA')
+    parser.add_argument("--dataset", default='cifar10', help='cifar10, celebA')
     opt = parser.parse_args()
+    print(opt)
     param = opt.__dict__
 
     iter_list = {'num_epochs': [100],
